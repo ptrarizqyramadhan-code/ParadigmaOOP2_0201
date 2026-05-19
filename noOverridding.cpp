e62cb0ed6ee2cb4590eae56b8e190365e7aa2631
ptrarizqyramadhan-code <ptrarizqyramadhan@gmail.com>
2026-05-19 15:23:20 +0700

    add explanation comments for overriding

--- noOverridding.cpp
+++ noOverridding.cpp
@@ -1,4 +1,16 @@
+#include <iostream>
+using namespace std;
 
+class baseClass
+{
+public:
+    virtual void perkenalan()
+    {
+        cout << "Hallo saya Function dari base class" << endl;
+    }
+};
+class darivedClass : public baseClass
+{
 public:
     void perkenalan1()
     {
