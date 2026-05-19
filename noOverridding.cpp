a373d1a88001381062ff97997f3ed9a8b4b7fc7c
ptrarizqyramadhan-code <ptrarizqyramadhan@gmail.com>
2026-05-19 15:36:04 +0700

    using namespace std;
    class baseClass final {
    public:
        virtual void perkenalan() {
            cout << "halo saya function dari baseClass";
        }
    };
    class baseClass1 {
    public:
        virtual void perkenalan() {
            cout << "halo saya function dari base Class";
        }
    };
    class darivedClass : public baseClass1 {
    public:
        virtual void perkenalan() {
            cout << "halo saya function dari derived Class";
        }

--- noOverridding.cpp
+++ noOverridding.cpp
@@ -1,9 +1,28 @@
+#include <iostream>
+using namespace std;
 
+class baseClass final {
+public:
+    virtual void perkenalan() {
+        cout << "halo saya function dari baseClass";
+    }
+};
+class baseClass1 {
+public:
+    virtual void perkenalan() {
+        cout << "halo saya function dari base Class";
+    }
+};
+class darivedClass : public baseClass1 {
+public:
+    virtual void perkenalan() {
+        cout << "halo saya function dari derived Class";
+    }
 };
 int main()
 {
     darivedClass a;
-    a.perkenalan1();
+    a.perkenalan();
 
     return 0;
 }
