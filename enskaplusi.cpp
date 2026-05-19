44ab948bee13ec5db80ffd39edf32de809918571
ptrarizqyramadhan-code <ptrarizqyramadhan@gmail.com>
2026-05-19 15:14:03 +0700

    add more lamp switch examples

--- enskaplusi.cpp
+++ enskaplusi.cpp
@@ -6,3 +6,14 @@ class remoteLampu
 private:
     string saklarNo[10];
 
+public:
+    void setSaklarNo(int i, string value)
+    {
+        saklarNo[i] = value;
+    }
+    string getSaklarNo(int i)
+    {
+        return saklarNo[i];
+    }   
+};
+
